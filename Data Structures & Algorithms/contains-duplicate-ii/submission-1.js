b157c5e3109f405f520bbf6074aa68a924436d3a
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-06-26 16:27:54 +0700

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        let m = new Map()
+
+        for(let i = 0; i < nums.length; i++) {
+            if(m.get(nums[i])) {
+                if(Math.abs(Number(m.get(nums[i])) - i) <= k) {
+                    return true
+                }
+                m.set(nums[i], i.toString())
+            } else {
+                m.set(nums[i], i.toString())
+            }
+        }
+        return false
+    }
+}
