71f4e384a64c969f54244e2c4a9c79aa962c4c3d
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-06-28 07:08:38 +0700

    Add: search-in-rotated-sorted-array-ii - submission-2

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-2.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        let i = 0
+        let j = nums.length - 1
+
+        while(i <= j) {
+            const m = i + Math.floor((j - i) / 2)
+
+            if(nums[m] === target) {
+                return true
+            }
+
+            if(
+                nums[i] === nums[m] &&
+                nums[m] === nums[j]
+            ) {
+                i++;
+                j--;
+                continue;
+            }
+
+            if(nums[m] >= nums[i]) {
+                if(target >= nums[m] || target < nums[i]) {
+                    i = m + 1
+                } else {
+                    j = m - 1
+                }
+            }  else {
+                if(target <= nums[m] || target > nums[j]) {
+                    j = m - 1
+                } else {
+                    i = m + 1
+                }
+            }
+        }
+        return false
+    }
+}
