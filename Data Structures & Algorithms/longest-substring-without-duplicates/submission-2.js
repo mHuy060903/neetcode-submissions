2ec63cf98811b2f37ee831adea6ab82d5376f6d6
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-07-15 09:14:11 +0700

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+       const m = new Map()
+       let count = 0
+       let l = 0
+
+       for(let r = 0; r < s.length; r++) {
+        if(m.has(s[r])) {
+            l = Math.max(l, m.get(s[r]) + 1)
+        }
+
+        m.set(s[r], r)
+
+        count = Math.max(count, r - l + 1)
+       }
+
+       return count
+
+    }
+}
