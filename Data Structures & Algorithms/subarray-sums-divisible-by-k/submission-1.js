a44810120281e42c02fef256568d263ead7b529f
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-08-03 07:22:10 +0700

    Add: subarray-sums-divisible-by-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sums-divisible-by-k/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraysDivByK(nums, k) {
+        let count = 0
+
+        for(let i = 0; i < nums.length ; i++) {
+            let curSum = 0;
+            for(let j = i ; j < nums.length; j++) {
+            curSum += nums[j];
+               if(s % k === 0) {
+                count +=1
+            }
+            }
+        }
+        return count
+    }
+}
