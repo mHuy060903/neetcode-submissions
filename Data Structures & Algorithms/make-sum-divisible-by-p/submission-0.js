b68ff7cc1010c6fe32aafe3ac54ccae4b76ef087
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-08-04 07:08:19 +0700

    Add: make-sum-divisible-by-p - submission-0

---
+++ Data Structures & Algorithms/make-sum-divisible-by-p/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} p
+     * @return {number}
+     */
+    minSubarray(nums, p) {
+        const sum = nums.reduce((acc, cur) => acc + cur, 0)
+
+        if(sum % p === 0) {
+            return 0
+        }
+
+        let min = 100001
+        let has = false
+
+        for(let i = 0; i < nums.length; i++) {
+            let t = sum
+            for(let j = i; j < nums.length; j++) {
+              t -= nums[j]
+              if(t % p === 0) {
+                has = true
+                min = Math.min(j - i + 1, min)
+              }
+            }
+        }
+
+        return has ? min : -1
+    }
+}
