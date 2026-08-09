9a1aa4b955c85e201f5e1bc4bb1c0772bae52a3e
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-08-09 07:49:25 +0700

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+        if(!root) {
+            return 0
+        }
+
+        let count = 1
+
+        function preOrder(node) {
+             if (root === null) {
+            return 0;
+        }
+
+        return (
+            1 + Math.max(this.maxDepth(root.left), this.maxDepth(root.right))
+        );
+        }
+    }
+}
