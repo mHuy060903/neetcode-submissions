b5968155ee137c31cc6bc8ba5d707497f32f5595
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-09-08 07:22:57 +0700

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        const maxPQ = new MaxPriorityQueue();
+ 
+        for(const s of stones) {
+            maxPQ.enqueue(s)
+        }
+
+        while(maxPQ.size() > 1) {
+            const stone1 = maxPQ.dequeue();
+            const stone2 = maxPQ.dequeue();
+
+            if (stone1 !== stone2) {
+                maxPQ.enqueue(stone1 - stone2);
+            }
+        }
+
+        return maxPQ.size() === 1 ? maxPQ.dequeue() : 0;
+    }
+}
