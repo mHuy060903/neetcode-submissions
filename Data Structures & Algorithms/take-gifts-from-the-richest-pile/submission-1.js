cc84afe2a02f854b841aeecf3a14a45a0ad2203d
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-09-09 07:21:04 +0700

    Add: take-gifts-from-the-richest-pile - submission-1

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} gifts
+     * @param {number} k
+     * @return {number}
+     */
+    pickGifts(gifts, k) {
+        const maxPQ = new MaxPriorityQueue()
+
+        for(let g of gifts) {
+            maxPQ.enqueue(g)
+        }
+
+        for(let i = 1; i <= k; i++) {
+            const n = Math.floor(Math.sqrt(maxPQ.dequeue()))
+            maxPQ.enqueue(n)
+        }
+        let total = 0
+        while(maxPQ.size()) {
+           total += maxPQ.dequeue()
+        }
+
+        return total
+    }
+}
