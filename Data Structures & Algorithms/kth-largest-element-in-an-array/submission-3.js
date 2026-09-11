6ab97f1609a8a121d9e31526d145f4329789337d
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-09-11 07:17:00 +0700

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        const maxPQ = new MaxPriorityQueue()
+
+        for(let n of nums) {
+            maxPQ.enqueue(n)
+        }
+
+        let m = k - 1
+        while(m > 0) {
+            maxPQ.dequeue()
+            m--
+        }
+
+        return maxPQ.front()
+    }
+}
