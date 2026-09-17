d4cda541915cb1901febcbb3af9937806172958f
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-09-17 13:20:21 +0700

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        const t = [...cost, 0]
+
+        for(let i = t.length - 3; i >= 0; i--) {
+          t[i] = Math.min(t[i] + t[i+1], t[i+2] + t[i])
+        }
+
+        return Math.min(t[0], t[1])
+    }
+}
