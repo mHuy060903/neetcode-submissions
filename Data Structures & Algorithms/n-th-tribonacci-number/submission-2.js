53ba1dbd821402c41de16d5ee4db958bc3d686c9
Minh Huy <103357557+mHuy060903@users.noreply.github.com>
2026-09-18 07:06:57 +0700

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    tribonacci(n) {
+         if (n <= 2) {
+            return n === 0 ? 0 : 1;
+        }
+        const t = new Array(n + 1).fill(0)
+        t[1] = 1
+        t[2] = 1
+
+        for(let i = 3; i < n; i++) {
+            t[i] = t[i-1] + t[i-2] + t[t-3] 
+        }
+
+        return t[n]
+     }
+}
